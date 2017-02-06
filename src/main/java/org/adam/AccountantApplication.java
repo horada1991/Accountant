5116c322ed564921c7a7481c33fa1702bb2e4cb8
Ádám Horváth <horada1991@gmail.com>
2017-02-06 18:19:23 +0100

    Setup SpringBoot

---
+++ src/main/java/org/adam/AccountantApplication.java
@@ -0,0 +1,12 @@
+package org.adam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountantApplication.class, args);
+	}
+}
